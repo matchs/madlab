f42284642c6fd34c45228fceeaef8f2f56d1906b
matchs <mateuschs@gmail.com>
2016-04-18 23:06:27 -0300

    Initial tree implementation

---
+++ data_structures/trees/bst/bst.js
@@ -0,0 +1,47 @@
+function _clone(node) {
+    return Object.assign(new Node(), node);
+}
+
+function Tree(_root) {
+    this.root = _clone(_root);
+
+    this.insert = function(_node_, _into_) {
+        var root = _into_ || this.root;
+        var node = _clone(_node_);
+        if(!root) {
+            root = node;
+            return this;
+        }
+
+        if(node.value < root.value) {
+            var tmp = _clone(this.root);
+
+            this.root = node;
+            this.root.leftChild = tmp;
+
+            return this;
+        }
+
+        if(node.value > root.value) {
+            return root.leftChild && node.value < root.leftChild ? this.insert(node, root.leftChild) : this.insert(node, root.rightChild);
+        }
+
+        return this;
+    }
+
+
+    this.traverseInOrder = function() {
+        return [];
+    }
+
+    this.traversePostOrder = function() {
+        return [];
+    }
+}
+
+function Node(value, parent, leftChild, rightChild) {
+    this.value = value;
+    this.parent = parent;
+    this.leftChild = leftChild;
+    this.rightChild = rightChild;
+}
